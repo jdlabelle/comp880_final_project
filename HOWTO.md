9ae11ac641044672be80b94eb589ab63bfff8625
jdlabelle <labelljd@gmail.com>
2023-04-29 21:05:37 -0400

    update client method, update HOWTO.md, close #7, #8, #9

---
+++ HOWTO.md
@@ -0,0 +1,24 @@
+## *Python Project Environment*
+Uses the standard python3 library (version 3.11). 
+* Ensure the following are installed:
+  * pip
+  * pytest
+
+## *Run Configuration Description*
+Three primary files:
+* Source Code `nhl_lines.py`
+  * not meant to run - import within another program to utilize the class.
+* client demonstration file `client.py`
+  * Run this to utilize the program and demo the methods of the Lines class imported from the source code `nhl_lines.py`
+  * Follow instructions prompted when you run the program!
+* three tests within the tests directory
+  * test both primary methods and the read-in helper method contained in `nhl_lines.py`. One file for each test. Must
+    have pip and pytest installed in order to run the tests.
+
+## *How to run the test cases*
+1. ensure pip and pytest packages are downloaded
+2. ensure you have the entire project cloned locally, as you will need to import from the source code in src directory, 
+   and you will need the data files in the data directory. Otherwise, the paths will need to be updated if the project 
+   file structure is changed.
+3. Run the test program!
+
--- src/client.py
+++ src/client.py
@@ -17,7 +17,6 @@ def offensive_lines_method():
     most_effective_lines = lines_obj.most_effective_offensive_lines()
     for rank, offensive_line in most_effective_lines.items():
         print("Rank:", rank, ' ', "Line:", offensive_line)
-    lines_obj.write_to_file()
 
 
 def defensive_pair_method():
@@ -30,6 +29,16 @@ def defensive_pair_method():
     for team, defensive_pair in most_physical_pair.items():
         pair, hits = defensive_pair
         print("Team:", team, ' ', "Pair:", pair, ' ', "Number of hits:", hits)
+
+
+def write_output():
+    """
+    Demonstrate `write_to_file()` method in Lines.
+    """
+    lines_obj = Lines("../data/NHL_Line_Data.csv", "../data/data_out.txt")
+    lines_obj.organize_by_line()
+    lines_obj.most_effective_offensive_lines()
+    lines_obj.most_physical_defensive_pair()
     lines_obj.write_to_file()
 
 
@@ -50,6 +59,13 @@ def main():
         elif choice not in ("A", "B", "Q"):
             print("please enter a valid choice\n")
             continue
+
+    choice2 = input("Would you like to test the write method?\n Enter Y if"
+                    " yes or N if no: ")
+    choice2 = choice2.upper()
+    if choice2 == "Y":
+        write_output()
+        print("Check the data_out.txt file in data!\n")
     print("Thank you for trying my program!")
 
 
